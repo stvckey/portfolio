835c83b5a196a1f0c3a6f8d8ec39d60335419a86
Ettione Stuckey II <97483875+stvckey@users.noreply.github.com>
2023-03-01 20:01:29 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -87,7 +87,7 @@ export default function Home() {
               <p className='text-2xl md:text-4xl py-5 leading-11 font-teko text-gray-500'>//get in touch</p>
             </a>
 
-             <a href="https://drive.google.com/file/d/1E2MQ2Bo_ih3e8eERFTtyLENq6fmm9rve/view?usp=sharing">
+             <a className="py-5 px-8" href="https://drive.google.com/file/d/1E2MQ2Bo_ih3e8eERFTtyLENq6fmm9rve/view?usp=sharing">
                <button className='text-xl bg-green-500 text-white font-tekobold duration-500 px-4 py-2 hover:bg-green-400 rounded'>RESUME</button>
              </a>
 
